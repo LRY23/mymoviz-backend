405a934f9ca2e9c14bdf2c7a73f260317542d963
Yann <yannleroy23@gmail.com>
2023-07-06 07:22:23 +0200

    deploiemenet sur vercel

--- urls.js
+++ urls.js
@@ -1,8 +1,8 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'http://mymoviz-backend-eight-gamma.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'http://mymoviz-frontend-flax.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
 
