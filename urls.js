6ea21f0280675096aac074a2c402cd0d41511261
Yann <yannleroy23@gmail.com>
2023-07-10 20:15:40 +0200

    ok

--- urls.js
+++ urls.js
@@ -1,8 +1,8 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'http://mymoviz-backend-eight-gamma.vercel.app';
+const BACKEND_URL = 'http://mymoviz-backend-mu-eight.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'http://mymoviz-frontend-flax.vercel.app';
+const FRONTEND_URL = 'http://mymoviz-frontend-lyart.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
 
