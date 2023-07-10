56f1ac5d9232fa7d5c8490f231f0bad62703103a
Yann <yannleroy23@gmail.com>
2023-07-10 20:02:10 +0200

    deploiement sur vercel

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
-const myApi = process.env.MY_API
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
-router.get('/movies', (req,res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${myApi}`)
-    .then(response => response.json())
-    .then(data => {  
-       res.json ({movies:data.results})
-    })
-})
+router.get('/movies', (req, res) => {
+	fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
+		.then(response => response.json())
+		.then(data => {
+			res.json({ movies: data.results });
+		});
+});
 
 module.exports = router;
